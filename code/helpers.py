e886fe2ccf69b7051097056e3c99b10d25cfef09
ARandomPotatoPerson <richardzhao2002@gmail.com>
2023-10-28 12:35:56 -0400

    Undersampling
    Built an undersampling method into the helpers setup

--- code/helpers.py
+++ code/helpers.py
@@ -2,6 +2,7 @@ import pandas as pd
 import numpy as np
 from sklearn.preprocessing import StandardScaler
 from imblearn.under_sampling import RandomUnderSampler
+from collections import Counter
 
 np.set_printoptions(suppress=True)
 #I refuse to try to read scientific notation
@@ -9,4 +10,8 @@ np.set_printoptions(suppress=True)
 def standardization(dataset):
     return StandardScaler().fit_transform(dataset)
 
-def undersampling(dataset):
+def undersampling(dataset, labels):
+    rus = RandomUnderSampler(random_state=0)
+    newDataset, newLabels = rus.fit_resample(dataset, labels)
+
+    return newDataset, newLabels
