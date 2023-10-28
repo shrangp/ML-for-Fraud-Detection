640b418fea14319f4e7c1105d86a9eeb5fd7d00e
ARandomPotatoPerson <richardzhao2002@gmail.com>
2023-10-28 12:25:41 -0400

    Uploading an updated version that reads off the zip file instead.

--- code/helpers.py
+++ code/helpers.py
@@ -1,9 +1,12 @@
 import pandas as pd
 import numpy as np
 from sklearn.preprocessing import StandardScaler
+from imblearn.under_sampling import RandomUnderSampler
 
 np.set_printoptions(suppress=True)
 #I refuse to try to read scientific notation
 
 def standardization(dataset):
     return StandardScaler().fit_transform(dataset)
+
+def undersampling(dataset):
